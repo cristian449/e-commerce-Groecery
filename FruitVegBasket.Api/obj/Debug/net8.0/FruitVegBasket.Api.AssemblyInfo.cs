543590c3fd1756b46a0bfc3e561aebc58cc2b663
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-03-20 11:50:47 +0200

    Pooleli!!!!Part8 Commit 17:22

--- FruitVegBasket.Api/obj/Debug/net8.0/FruitVegBasket.Api.AssemblyInfo.cs
+++ FruitVegBasket.Api/obj/Debug/net8.0/FruitVegBasket.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FruitVegBasket.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f6c0c033f3f1ac7629f075d740ec8eeff92cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36efb1f3937a7c4e7155c8b2e7bb4a89359c3636")]
 [assembly: System.Reflection.AssemblyProductAttribute("FruitVegBasket.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FruitVegBasket.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
